136d7a65c121edd17b2fafba599cc4c7c6ee3b3c
alorb <70232593+alorb@users.noreply.github.com>
2025-12-03 21:54:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 data "tfe_outputs" "source_workspace" {
   workspace    = var.workspace_name
   organization = var.organization_name
