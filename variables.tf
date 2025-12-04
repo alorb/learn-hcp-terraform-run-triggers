b32ba35f7ebedb3dc8b232ba46378b253a15a089
alorb <70232593+alorb@users.noreply.github.com>
2025-12-03 21:54:51 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "instance_name" {
-  description = "Value of the EC2 instance's Name tag."
-  type        = string
-  default     = "learn-hcp-terraform-run-triggers"
-}
-
-variable "instance_type" {
-  description = "The EC2 instance's type."
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "workspace_name" {
   description = "Name of the source workspace to query."
   type        = string
